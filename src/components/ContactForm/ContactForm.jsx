0ac07d8e8aa2cc2a1bcfa8fdc90247e9306c4a5b
Sultan <a_sultan_93@mail.ru>
2023-05-22 12:01:29 +0500

    1111

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,26 +1,31 @@
-import { Component } from "react";
+import React, { Component } from 'react';
+
 import PropTypes from 'prop-types';
+
 import { Formik, Form, ErrorMessage } from 'formik';
 
+import { Button, Input, Label } from './ContactForm.styled';
+
 class ContactForm extends Component {
-    state = {
-        name: '',
-        number: '',
-    };
+  state = {
+    name: '',
+    number: '',
+  };
 
   handleSubmit = (values, action) => {
     this.props.onSubmit(values);
     action.resetForm();
   };
 
-    render(){
-        const { name, number } = this.state;
-        return(
-            <Formik initialValues={{ name, number }} onSubmit={this.handleSubmit}>
+  render() {
+    const { name, number } = this.state;
+
+    return (
+      <Formik initialValues={{ name, number }} onSubmit={this.handleSubmit}>
         <Form>
-          <label>
+          <Label>
             Name
-            <input
+            <Input
               type="text"
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
@@ -28,23 +33,23 @@ class ContactForm extends Component {
               required
             />
             <ErrorMessage name="name" component="div" />
-          </label>
-          <label>
+          </Label>
+          <Label>
             Number
-            <input
+            <Input
               type="tel"
               name="number"
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
             />
-          </label>
+          </Label>
           <ErrorMessage name="number" component="div" />
-          <button type="submit">Add contact</button>
+          <Button type="submit">Add contact</Button>
         </Form>
       </Formik>
-        );
-    }
+    );
+  }
 }
 
 export default ContactForm;
